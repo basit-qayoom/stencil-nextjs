6afe934ebc7c362828da626da66ef6e7bcfa689a
Basit Qayoom Chowdhary <basitqayoomchowdhary@Basit-12905.local>
2023-08-10 15:02:14 +0530

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { defineCustomElements, applyPolyfills } from "stencil-pw/loader";
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    applyPolyfills().then(() => {
+      defineCustomElements(window);
+    });
+  }, []);
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Stencil NextJS</h1>
+      <my-form></my-form>
+      <my-timer></my-timer>
+      <my-input></my-input>
+      <my-button></my-button>
+      <text-field text="Hi from stencil & next"></text-field>
+    </>
+  );
+}
