630ed2362d3c81708fc896b7d187fead1b6c1be8
Basit Qayoom Chowdhary <basitqayoomchowdhary@Basit-12905.local>
2023-08-21 11:13:21 +0530

    individual components added and shadow root disabled

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,28 @@ export default function Home() {
     <>
     <h1>Next App</h1>
       <my-app></my-app>
+      <hr />
+      <h1>------TESTING OTHER COMPONENTS::::::</h1>
+      <hr />
+      <hr />
+      <hr />
+      <pw-card link='Link as prop' card-title='Title from Next'>
+        <a href='https://www.google.com'>Slot Link: https://www.google.com</a>
+      </pw-card>
+      <hr />
+      <my-timer></my-timer>
+      <hr />
+      <my-button></my-button>
+      <hr />
+      <my-form></my-form>
+      <hr />
+      <my-input></my-input>
+      <hr />
+      <text-field text='Hello from text stencil component--- hey basit'></text-field>
+      <hr />
+      <hr />
+      <hr />
     </>
   );
 }
+
